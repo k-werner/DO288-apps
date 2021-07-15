1dec8888bdcefef98497ba82774a89e908f9a995
Kevin Werner <wernke@akros.ch>
2021-07-15 21:38:56 +0200

    template and new-app shell script

---
+++ quotes/create-app.sh
@@ -0,0 +1,9 @@
+oc new-app --as-deployment-config --template wernke-common/php-mysql-ephemeral  \
+    -p NAME=quotesapi \
+	-p APPLICATION_DOMAIN=quote-wernke.apps-crc.testing \
+	-p SOURCE_REPOSITORY_URL=https://github.com/k-werner/DO288-apps \
+	-p CONTEXT_DIR=quotes \
+	-p DATABASE_SERVICE_NAME=quotesdb\
+	-p DATABASE_USER=user1 \
+	-pDATABASE_PASSWORD=mypa55 \
+	--name quotes
